ba4a646d551693e6d4bdbbe611ccd64c76241283
Vikram Pasupathy <vpasupathy@ucsb.edu>
2020-05-29 09:50:37 -0700

    work

--- bulkLoading.py
+++ bulkLoading.py
@@ -1,2 +1,31 @@
-def bulkLoading():
+
+import csv
+import scipy
+import numpy as np
+import pandas as pd 
+import threading
+import generateRandomSequence as g
+import threading
+
+holder = []
+def bulkLoading(sequence):
+    holder = mymergesort(sequence)
+    return holder
+def mymergesort(sequence):
+    mid = len(sequence)/2
+    flushholder(holder)
+    return np.sort(sequence)
+def flushholder(holder):
+    pd.DataFrame(holder).to_csv("backup.csv")
+flushholder
+holder = g.generateRandomSequence()
+holder = bulkLoading(holder)
+for i in holder:
+    print(i)
+
+
+
+
+
+    
     
--- generateRandomSequence.py
+++ generateRandomSequence.py
@@ -1,1 +1,11 @@
+import sys
+import struct
+import numpy as np
+
+
+
 def generateRandomSequence():
+    return np.random.randint(2**32 , size = 1000000)
+
+holder = generateRandomSequence()
+
