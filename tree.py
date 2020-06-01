9ecd91384b60b9e1f627e4ae834038fcb60854ed
Vikram Pasupathy <vpasupathy@ucsb.edu>
2020-06-01 08:35:59 -0700

    need to implement tree and tree functions

--- tree.py
+++ tree.py
@@ -1,11 +1,7 @@
 import csv
 class InternalNode():
-    value = 
+    value = -1
     children = []
-    def construct(inputvalue):
-        value = inputvalue
-    def addChild(child):
-        children.append(child)
     
     
 class LeafNodePages():
@@ -13,15 +9,61 @@ class LeafNodePages():
     left = None
     right = None 
 
+
 holder = []
 count = 0
+number = 0
 
 with open('backup.csv') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         if(count > 0):
-            holder.append(row[1])
+            if(count == 1):
+                number = len(row)
+                leafnode = LeafNodePages()
+                leafnode.values = row[1]
+                holder.append(leafnode)
+                print(leafnode.values)
+        
+                count+=1
+            else:
+                number = len(row)
+                leafnode = LeafNodePages()
+                leafnode.values = row[1]
+                holder[count-2].right = leafnode
+                leafnode.left = holder[count-2]
+                holder.append(leafnode)
+                print(leafnode.left.values)
+                count+=1
+
+            
         else:
             count+=1
+
+# root = None
+# while(len(holder) > 0):
+#     length = len(holder)
+#     if(length == 1):
+#         root = holder[0]
+#     count = 0
+#     newholder = []
+#     for i in range(length):
+#         if(count == number):
+#             newholder.append(newholder)
+#             count == 0
+#         internal = InternalNode()
+#         internal.children.append(holder[i])
+#         count +=1
+#     holder = newholder
+
+
+
+
+
+
+        
+
+
+
 
 
