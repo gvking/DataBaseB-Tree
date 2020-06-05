ff1b65ab1885f34d4cc3832c64026e1046f9f791
Vikram Pasupathy <vpasupathy@ucsb.edu>
2020-06-05 09:16:16 -0700

    work

--- tree.py
+++ tree.py
@@ -18,79 +18,80 @@ def searchforval(root, val):
     if hasattr(root, 'children'):
         while root.children:
             if( val < float(root.children[0].value)):
-                searchforval(root.children[0], val)
+                return searchforval(root.children[0], val)
                 break          
             for i in range(1, len(root.children)):
                 if(val >= float(root.children[i-1].value) and val < float(root.children[i].value)):
-                    searchforval(root.children[i-1], val)
-                    searchforval(root.children[i], val)   
+                    return searchforval(root.children[i-1], val)
+                    return searchforval(root.children[i], val)   
     if hasattr(root, 'actvalues'):
         for i in root.actvalues:
             if (float(i) == float(val)):
-                print("hi")
+                return True
 
 
+def createtree():
+    holder = []
+    count = 0
+    number = 0
 
-holder = []
-count = 0
-number = 0
-
-with open('backup.csv') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
-        if(count > 0):
-            if(count == 1):
-                number = len(row) - 1
-                leafnode = LeafNodePages()
-                leafnode.actvalues = row[1:]
-                [float(i) for i in leafnode.actvalues]
-                leafnode.value = leafnode.actvalues[int(len(leafnode.actvalues)/2)]
-                holder.append(leafnode)
-                #print(leafnode.values)
+    with open('backup.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            if(count > 0):
+                if(count == 1):
+                    number = len(row) - 1
+                    leafnode = LeafNodePages()
+                    leafnode.actvalues = row[1:]
+                    [float(i) for i in leafnode.actvalues]
+                    leafnode.value = leafnode.actvalues[int(len(leafnode.actvalues)/2)]
+                    holder.append(leafnode)
+                    #print(leafnode.values)
         
+                    count+=1
+                else:
+                    leafnode = LeafNodePages()
+                    leafnode.actvalues = row[1:]
+                    for i in leafnode.actvalues:
+                        if(i != ''):
+                            float(i)
+                        else:
+                            del i
+                    leafnode.value = leafnode.actvalues[int(len(leafnode.actvalues)/2)]
+                    holder[count-2].right = leafnode
+                    leafnode.left = holder[count-2]
+                    holder.append(leafnode)
+                    #print(leafnode.left.values)
+                    count+=1 
+            else:
                 count+=1
-            else:
-                leafnode = LeafNodePages()
-                leafnode.actvalues = row[1:]
-                for i in leafnode.actvalues:
-                    if(i != ''):
-                        float(i)
-                    else:
-                        del i
-                leafnode.value = leafnode.actvalues[int(len(leafnode.actvalues)/2)]
-                holder[count-2].right = leafnode
-                leafnode.left = holder[count-2]
-                holder.append(leafnode)
-                #print(leafnode.left.values)
-                count+=1 
-        else:
-            count+=1
 
-print(len(holder))
-count = 0
-num = len(holder)
-print(number)
-while(num > 1):
-    num = num/number
-    num = math.ceil(num)
-    newholder = []
-    print(num)
-    for i in range(num):
-        print(i)
-        newinternal = InternalNode()
-        for k in range(count, count+num):
-            if(k >= len(holder)):
-                break
-            else:
-                newinternal.children.append(holder[k])
-        count = count+num
-        newinternal.value = newinternal.children[int(len(newinternal.children)/2)].value
-        newholder.append(newinternal)
 
-    holder = newholder
-root = holder[0]
+    count = 0
+    num = len(holder)
+
+    while(num > 1):
+        num = num/number
+        num = math.ceil(num)
+        newholder = []
+        print(num)
+        for i in range(num):
+            newinternal = InternalNode()
+            for k in range(count, count+num):
+                if(k >= len(holder)):
+                    break
+                else:
+                    newinternal.children.append(holder[k])
+            count = count+num
+            newinternal.value = newinternal.children[int(len(newinternal.children)/2)].value
+            newholder.append(newinternal)
+
+        holder = newholder
+    return holder[0]
+root = createtree()
 print(root.value)
-searchforval(root, 445)
+boolean = searchforval(root, 2148724404.0)
+print(boolean)
 
 
         
